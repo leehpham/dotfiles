28982851335eb4462553309dcf686ce019e6952e
Hoang Pham <hoang.pham@team.enouvo.com>
2023-03-14 11:05:56 +0700

    neovim: added indent-blankline plugin.

--- nvim/lua/leehpham/plugins-setup.lua
+++ nvim/lua/leehpham/plugins-setup.lua
@@ -91,6 +91,8 @@ return packer.startup(function(use)
   use {"saadparwaiz1/cmp_luasnip"}
   use {"rafamadriz/friendly-snippets"}
 
+  -- Indentation guides
+  use {"lukas-reineke/indent-blankline.nvim"}
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
