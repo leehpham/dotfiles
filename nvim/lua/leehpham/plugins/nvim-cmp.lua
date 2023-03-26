7c45698cc53584ba8231ab6d743c993ce9eb2106
Hoang Pham <lehoangpham1092@gmail.com>
2023-03-26 16:44:32 +0700

    neovim: cmp

--- nvim/lua/leehpham/plugins/nvim-cmp.lua
+++ nvim/lua/leehpham/plugins/nvim-cmp.lua
@@ -7,3 +7,20 @@ local luasnip_status, luasnip = pcall(require, "luasnip")
 if not luasnip_status, then
   return
 end
+
+-- load friendly-snippets
+require("luasnip/loaders/from_vscode").lazy_load()
+
+-- completeopt's default values are "menu,preview"
+vim.opt.completeopt = "menu,menuone,noselect"
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.present.insert({
+    ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+  })
+})
